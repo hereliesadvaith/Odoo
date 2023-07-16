4126368a6d4b4fb2d811f8a587614d2cc59c0b3c
hereliesadvaith <hereliesadvaith@gmail.com>
2023-07-16 22:50:03 +0530

    productTemplateViews

--- warranty/__manifest__.py
+++ warranty/__manifest__.py
@@ -11,6 +11,7 @@
         "data/warranty_sequence.xml",
         "views/request_for_warranty_views.xml",
         "views/warranty_type_views.xml",
+        "views/product_template_views.xml",
         "views/warranty_menus.xml",
     ],
     "installable": True,
