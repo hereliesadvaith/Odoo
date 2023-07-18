397a5434e5e2e6c3c9b1418dd257c8e0d32216b7
hereliesadvaith <hereliesadvaith@gmail.com>
2023-07-19 03:21:28 +0530

    bugFixes

--- warranty/models/product_template.py
+++ warranty/models/product_template.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 from odoo import fields, models
 
 
@@ -6,12 +7,16 @@ class ProductsTemplate(models.Model):
     """
     To add fields to product template model.
     """
+
     _inherit = "product.template"
 
     has_warranty = fields.Boolean(string="Has Warranty")
-    warranty_type = fields.Selection(string="Warranty Type", selection=[
-        ('service_warranty', 'Service Warranty'),
-        ('replacement_warranty', 'Replacement Warranty'),
-    ], required=True)
-    warranty_period = fields.Integer(string="Warranty Period(Days)",
-                                     required=True)
+    warranty_type = fields.Selection(
+        string="Warranty Type",
+        selection=[
+            ("service_warranty", "Service Warranty"),
+            ("replacement_warranty", "Replacement Warranty"),
+        ],
+        required=True,
+    )
+    warranty_period = fields.Integer(string="Warranty Period(Days)", required=True)
