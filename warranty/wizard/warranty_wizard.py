1bb4eb8a50b10fc47bc3cdf95e3601ce90d9d766
Advaith B G <hereliesadvaith@gmail.com>
2023-08-04 15:18:56 +0530

    warrantyReport

---
+++ warranty/wizard/warranty_wizard.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class WarrantyReport(models.TransientModel):
+    """
+    Model for wizard
+    """
+    _name = "warranty.wizard"
+    _description = "Warranty Report"
+
+    product_ids = fields.Many2many("product.product",
+                                   string="Product",
+                                   help="Product")
+    partner_id = fields.Many2one("res.partner")
+    start_date = fields.Date(string="Start Date")
+    end_date = fields.Date(string="End Date")
+    warranty_id = fields.Many2one("request.for.warranty")
+
+    # Action methods
+
+    def action_confirm(self):
+        """
+        To get the report
+        """
+        pass
