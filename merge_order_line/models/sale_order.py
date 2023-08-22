02683c50d3e33384beba72682c1170e413d2ed02
Advaith <hereliesadvaith@gmail.com>
2023-08-22 23:01:26 +0530

    updations

--- merge_order_line/models/sale_order.py
+++ merge_order_line/models/sale_order.py
@@ -8,35 +8,23 @@ class SaleOrder(models.Model):
     """
     _inherit = "sale.order"
 
-    # CRUD Methods
+    # Constrains
 
-    @api.model
-    def create(self, vals):
+    @api.constrains("order_line")
+    def _merge_order_line(self):
         """
         To merge sale order lines with same product
         """
-        product_id_list = []
-        for i in vals["order_line"]:
-            if ([i[2]["product_id"], i[2]["price_unit"]]
-                    in [[i[0], i[2]] for i in product_id_list]):
-                for j in product_id_list:
-                    if (i[2]["product_id"] == j[0]) and (
-                            i[2]["price_unit"] == j[2]):
-                        j[1] += i[2]["product_uom_qty"]
-            else:
-                product_id_list.append(
-                    [i[2]["product_id"], i[2]["product_uom_qty"], i[2][
-                        "price_unit"]])
-        order_line_copy = []
-        for j in product_id_list:
-            for i in vals["order_line"]:
-                if (j[0] == i[2]["product_id"]) and (
-                        j[2] == i[2]["price_unit"]):
-                    if [i[2]["product_id"], i[2]["price_unit"]] not in [
-                        [k[2]["product_id"], k[2][
-                            "price_unit"]] for k in order_line_copy]:
-                        i[2]["product_uom_qty"] = j[1]
-                        order_line_copy.append(i)
-        vals["order_line"] = order_line_copy
-        res = super(SaleOrder, self).create(vals)
-        return res
+        for order in self.order_line:
+            if order.id in self.order_line.ids:
+                order_ids = self.order_line.filtered(
+                    lambda r: r.product_id == order.product_id and
+                    r.price_unit == order.price_unit
+                )
+                quantity = 0
+                unit_price = order_ids[0].price_unit
+                for record in order_ids:
+                    quantity += record.product_uom_qty
+                order_ids[0].product_uom_qty = quantity
+                order_ids[0].price_unit = unit_price
+                order_ids[1:].unlink()
