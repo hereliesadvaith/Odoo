5c68d4abf343816233984edc783adbd4e143a93f
Advaith B G <hereliesadvaith@gmail.com>
2023-09-30 14:42:54 +0530

    website_order_tracking

---
+++ website_order_tracking/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Website Order Tracking",
+    "version": "16.0.1.0",
+    "author": "Advaith",
+    "category": "Website",
+    "summary": "Tracking of orders in website.",
+    "description": """
+    This module will show the details of the delivery of sales
+    orders.
+    """,
+    "depends": ["base", "sale", "website", "stock"],
+    "data": [
+    ],
+    "installable": True,
+    "license": "LGPL-3",
+}
---
+++ website_order_tracking/controllers/main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+
+
+class WebsiteTracking(http.Controller):
+    """
+    To inherit add values in website tracking page
+    """
