133b040b3ba714c319da690e46b48827673f1fa2
Advaith B G <hereliesadvaith@gmail.com>
2023-11-02 10:32:22 +0530

    [IMP] monthly_sale_report: optimization

---
+++ monthly_sale_report/README.md
@@ -0,0 +1,4 @@
+# Monthly Sales Report
+
+![Static Badge](https://img.shields.io/badge/Status-In_Development-red)
+![Static Badge](https://img.shields.io/badge/Odoo-16.0-darkviolet)
--- monthly_sale_report/models/monthly_sale_report.py
+++ monthly_sale_report/models/monthly_sale_report.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models
 import base64
+import datetime
+from dateutil.relativedelta import relativedelta
 
 
 class MonthlySaleReport(models.Model):
@@ -43,25 +45,35 @@ class MonthlySaleReport(models.Model):
         """
         self.state = "paused"
 
-    def cron_monthly_sale_report(self):
+    def send_report(self, period, start_date, end_date):
         """
-        Cron function to send monthly report.
+        To get data for report
         """
         company_id = self.env.context['allowed_company_ids'][0]
         query = f"""
         SELECT slo.name AS reference, rsp.name AS customer,
-        slo.amount_total AS amount, slo.date_order AS date,
+        slo.amount_total AS amount, DATE(slo.date_order) AS date,
         slo.state AS status FROM sale_order AS slo
         INNER JOIN res_partner AS rsp ON
         rsp.id = slo.partner_id
-        WHERE slo.company_id = {company_id}
+        INNER JOIN crm_team AS ctm ON
+        ctm.id = slo.team_id
+        WHERE slo.company_id = {company_id} AND
+        DATE(slo.date_order) BETWEEN '{start_date}' AND '{end_date}'
         """
         for record in self.search([("state", "=", "ongoing"),
-                                   ("period", "=", "month")]):
+                                   ("period", "=", period)]):
+            if record.team_id:
+                query += f"""
+                AND ctm.id = {record.team_id.id}
+                """
             self.env.cr.execute(query)
             sale_orders = self.env.cr.dictfetchall()
             data = {
                 "sale_orders": sale_orders,
+                "start_date": start_date.strftime('%b %d %Y'),
+                "end_date": end_date.strftime('%b %d %Y'),
+                "sale_team": record.team_id.name if record.team_id else False
             }
             sale_report = self.env.ref(
                 'monthly_sale_report.pdf_sale_report_action'
@@ -99,58 +111,19 @@ class MonthlySaleReport(models.Model):
                         sale_report_attachment_id.id
                     )]
 
+
+    def cron_monthly_sale_report(self):
+        """
+        Cron function to send monthly report.
+        """
+        end_date = datetime.date.today()
+        start_date = end_date - relativedelta(months=1)
+        self.send_report("month", start_date, end_date)
+
     def cron_weekly_sale_report(self):
         """
         Cron function to send weekly report.
         """
-        company_id = self.env.context['allowed_company_ids'][0]
-        query = f"""
-        SELECT slo.name AS reference, rsp.name AS customer,
-        slo.amount_total AS amount, slo.date_order AS date,
-        slo.state AS status FROM sale_order AS slo
-        INNER JOIN res_partner AS rsp ON
-        rsp.id = slo.partner_id
-        WHERE slo.company_id = {company_id}
-        """
-        for record in self.search([("state", "=", "ongoing"),
-                                   ("period", "=", "week")]):
-            self.env.cr.execute(query)
-            sale_orders = self.env.cr.dictfetchall()
-            data = {
-                "sale_orders": sale_orders,
-            }
-            sale_report = self.env.ref(
-                'monthly_sale_report.pdf_sale_report_action'
-            )
-            data_record = base64.b64encode(
-                self.env['ir.actions.report'].sudo()._render_qweb_pdf(
-                    sale_report, [record.id], data=data
-                )[0]
-            )
-            ir_values = {
-                'name': 'Sale Report',
-                'type': 'binary',
-                'datas': data_record,
-                'store_fname': data_record,
-                'mimetype': 'application/pdf',
-                'res_model': 'monthly.sale.report',
-            }
-            sale_report_attachment_id = self.env[
-                'ir.attachment'].sudo().create(
-                ir_values)
-            if sale_report_attachment_id:
-                email_template = self.env.ref(
-                    'monthly_sale_report.email_template_sale_report')
-                email_template.attachment_ids = [sale_report_attachment_id.id]
-                emails = [i.email for i in record.partner_ids]
-                if email_template and emails:
-                    email_values = {
-                        'email_to': emails[0],
-                        'email_from': self.env.user.email,
-                        'email_cc': emails[1:],
-                    }
-                    email_template.send_mail(
-                        self.id, email_values=email_values, force_send=True)
-                    email_template.attachment_ids = [fields.Command.unlink(
-                        sale_report_attachment_id.id
-                    )]
+        end_date = datetime.date.today()
+        start_date = end_date - relativedelta(weeks=1)
+        self.send_report("week", start_date, end_date)
