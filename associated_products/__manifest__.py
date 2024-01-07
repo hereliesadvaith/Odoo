acbd1fba50c42a30c268bf0b26b71199a06a1a20
Advaith B G <hereliesadvaith@gmail.com>
2024-01-07 23:51:15 +0530

    [REL] asscociated_products: v1.0.0

---
+++ associated_products/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Associated Products",
+    "version": "17.0.1.0.0",
+    "category": "Sales",
+    "summary": "Adding associated products to customers.",
+    "description": """
+    This module is used to associate specific products to customers.
+    """,
+    "author": "Advaith",
+    "website": "https://hereliesadvaith.github.io",
+    "depends": ["base", "sale"],
+    "data": [
+        "views/res_partner_views.xml",
+        "views/sale_order_views.xml",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+}
