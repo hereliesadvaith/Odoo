e284e03ff0356a9ba295432001f66e67ae7a77d0
Advaith B G <hereliesadvaith@gmail.com>
2025-09-19 22:50:57 +0530

    [ADD] odoo_mcp_server

---
+++ odoo_mcp_server/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Odoo MCP Server",
+    "version": "19.0.1.0.0",
+    "category": "Tools",
+    "summary": "MCP server for odoo.",
+    "description": "MCP server for odoo.",
+    "author": "Advaith B G",
+    "website": "https://hereliesadvaith.github.io",
+    "depends": ["base", "mail", "im_livechat"],
+    "data": [],
+    "license": "LGPL-3",
+    "installable": True,
+}
