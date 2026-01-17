63a551d50b9af5ef20192c6ab39a36f18079533c
Advaith B G <hereliesadvaith@gmail.com>
2026-01-17 15:43:41 +0530

    [ADD] n8n_chatbot

---
+++ n8n_chatbot/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "N8N Chatbot",
+    "version": "19.0.1.0.0",
+    "category": "Tools",
+    "summary": "N8N Chatbot.",
+    "description": "N8N Chatbot.",
+    "author": "Advaith B G",
+    "website": "https://hereliesadvaith.github.io",
+    "depends": ["base", "mail", "contacts", "im_livechat"],
+    "data": [
+        "data/ir_action_server.xml",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+}
